2f7ed03b8b71ef10fa58d66e504f289b413543eb
deyk <deyk@crossway.org>
2011-05-15 20:47:46 -0700

    Deployment tasks.

--- pavement.py
+++ pavement.py
@@ -5,7 +5,9 @@ from paver.easy import *
 
 
 @task
-def sync():
+def s3sync():
+    """Sync the files to S3/Cloudfront.
+    """
     sh(
         ('s3cmd sync ./ s3://cache.doesitrockyet.com '
          '--recursive --exclude=".git*" '
@@ -13,3 +15,18 @@ def sync():
          '--add-header="Cache-Control:public"'
          )
         )
+
+
+@task
+def sync():
+    """Sync the files to the webhost.
+    """
+    sh('rsync -ravz ./ --exclude=".git*" eykd@doesitrockyet.com:webapps/com_doesitrockyet__static/')
+
+
+@task
+@needs('s3sync', 'sync')
+def deploy():
+    """Deploy the site.
+    """
+    pass
